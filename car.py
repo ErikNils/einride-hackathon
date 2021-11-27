5d4e6e813431ad6938085fd89721a9347416c32c
McShrekt <erik1.nilsson2@gmail.com>
2021-11-27 14:34:47 +0100

    Ignores the upper third of the video Co-authored-by: Fredrik93 fredrikullman93@gmail.com

--- car.py
+++ car.py
@@ -1,6 +1,7 @@
 import websocket
 import _thread
 import time
+import numpy as np
 import cv2
 
 host = "192.168.43.243"
@@ -20,11 +21,25 @@ def on_error(ws, error):
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
+
+
+
+
 def CannyEdge(image):
-  gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-  blur = cv2.GaussianBlur(gray, (5,5), 0)
-  cannyImage = cv2.Canny(blur, 60, 180)
-  return cannyImage
+    gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray, (5,5), 0)
+    cannyImage = cv2.Canny(blur, 50, 150)
+    return cannyImage
+
+def region_of_interest(image):
+    height = image.shape[0]
+    rectangle = np.array([[(0, height),(0, 40), (160,40), (160, height),]], np.int32)
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, rectangle, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
+
+
 
 
 def on_open(ws):
@@ -39,30 +54,21 @@ def on_open(ws):
 
         capture = cv2.VideoCapture(video_address)
   
-        while(True):
-
-            ret, frame = capture.read()
-
-            grayFrame = CannyEdge(frame)
-
-            cv2.imshow('video gray', grayFrame)
-            cv2.imshow('video original', frame)
-
-            if cv2.waitKey(1) == 27:
-                break
-  
-        capture.release()
-        cv2.destroyAllWindows()
+        
 
         while True:
             ret, frame = cap.read()
 
             gray = CannyEdge(frame)
 
+            cropped_video = region_of_interest(gray)
+            
+
             cv2.imshow('video gray', gray)
             cv2.imshow('video original', frame)
+            cv2.imshow("Final video", cropped_video)
 
-            if cv2.waitKey(1) == 27:
+            if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
 
             # do something based on the frame
@@ -72,6 +78,12 @@ def on_open(ws):
             message = f"{{\"angle\":{angle},\"throttle\":{throttle},\"drive_mode\":\"user\",\"recording\":false}}"
             ws.send(message)
             print(message)
+
+        angle = 0.0
+        throttle = 0.0
+        message = f"{{\"angle\":{angle},\"throttle\":{throttle},\"drive_mode\":\"user\",\"recording\":false}}"
+        ws.send(message)
+        print(message)
   
         capture.release()
         cv2.destroyAllWindows()
