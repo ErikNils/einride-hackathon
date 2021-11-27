69c9c23e03ad67750cc1e02e787748ca520d12c3
McShrekt <erik1.nilsson2@gmail.com>
2021-11-27 13:05:25 +0100

    Can now detect lines, not red lines sadly

--- car.py
+++ car.py
@@ -3,7 +3,7 @@ import _thread
 import time
 import cv2
 
-host = "donkeycar"
+host = "192.168.43.243"
 port = 8887
 
 socket_address = f"ws://{host}:{port}/wsDrive"
@@ -20,6 +20,12 @@ def on_error(ws, error):
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
+def CannyEdge(image):
+  gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+  blur = cv2.GaussianBlur(gray, (5,5), 0)
+  cannyImage = cv2.Canny(blur, 60, 180)
+  return cannyImage
+
 
 def on_open(ws):
     def run(*args):
@@ -31,15 +37,44 @@ def on_open(ws):
         height = frame.shape[0]
         width = frame.shape[1]
 
+        capture = cv2.VideoCapture(video_address)
+  
+        while(True):
+
+            ret, frame = capture.read()
+
+            grayFrame = CannyEdge(frame)
+
+            cv2.imshow('video gray', grayFrame)
+            cv2.imshow('video original', frame)
+
+            if cv2.waitKey(1) == 27:
+                break
+  
+        capture.release()
+        cv2.destroyAllWindows()
+
         while True:
             ret, frame = cap.read()
+
+            gray = CannyEdge(frame)
+
+            cv2.imshow('video gray', gray)
+            cv2.imshow('video original', frame)
+
+            if cv2.waitKey(1) == 27:
+                break
+
             # do something based on the frame
             angle = 0.0
-            throttle = 0.2
+            throttle = 0.0
 
             message = f"{{\"angle\":{angle},\"throttle\":{throttle},\"drive_mode\":\"user\",\"recording\":false}}"
             ws.send(message)
             print(message)
+  
+        capture.release()
+        cv2.destroyAllWindows()
 
     _thread.start_new_thread(run, ())
 
